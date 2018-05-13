d6504a9ce0387d1f1802733d39c597f180cd77b4
rachel <rachel.black@yoti.com>
2018-05-12 22:20:52 -0400

    step4

--- screens/regisitration/Step4.js
+++ screens/regisitration/Step4.js
@@ -4,56 +4,47 @@ import { Button } from "react-native-elements";
 import NavBar from "./../../components/NavBar";
 import styles from "./../../styles/style";
 
-const subfoods = [
-  "Asparagus",
-  "String Beans",
-  "Shell Beans",
-  "Beets",
-  "Beet Greens",
-  "Broccoli",
-  "Brussels Sprout",
-  "Cabbage",
-  "Carrots",
-  "Cauliflower",
-  "Celery",
-  "Collard Greens",
-  "Corn",
-  "Cucumbers"
-];
 
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
+
+// const numbers = ['1','2','3','4','5','6','7','8','9','10']
+const numbers =['pounds', 'kgs']
 export default class Step4 extends React.Component {
   constructor(props) {
     super(props);
-    console.log("step4", props.navigation.state.params)
+    console.log("step4 ", props.navigation.state.params)
     const state =  props.navigation.state.params
-    this.state = Object.assign({}, state, { subfood : "" });
+    this.state = Object.assign({}, state, { quanity : "1", measurement: 'pounds' });
     console.log("step4 state", this.state)
   }
-
+  
   render() {
     const { navigate } = this.props.navigation;
-
+    
     return (
       <View style={styles.container}>
-        <NavBar />
-        <Text style={styles.stepText}>STEP 4</Text>
-        <Text style={styles.heading2}>{`What kind of ${this.state.food} ?`}</Text>
-
+      <NavBar />
+      <Text style={styles.stepText}>STEP 4</Text>
+      <Text style={styles.heading}>{`How many ?`}</Text>
+      
+      <FormLabel>Amount</FormLabel>
+        <FormInput 
+          onChangeText={(quanity)=>(this.setState({ quanity }))} 
+          style={{ width : 100 }}/>
         <Picker
-          selectedValue={this.state.subfood}
-          style={{ height: 50, width: 100 }}
+          selectedValue={this.state.measurement}
+          style={{ height: 50, width: 200 }}
           onValueChange={(itemValue, itemIndex) =>
-            this.setState({ subfood: itemValue })
+            this.setState({ measurement: itemValue })
           }
         >
-          {subfoods.map(food => (
-            <Picker.Item key={food} label={food} value={food} />
+          {numbers.map(number => (
+            <Picker.Item key={number} label={number} value={number} />
           ))}
         </Picker>
         <Button
-          onPress={() =>
-            navigate("Reg5", this.state)
-          }
+          onPress={() => navigate('Reg5', this.state)}
           title="Next Step"
           large={true}
           backgroundColor="#1dc890"
